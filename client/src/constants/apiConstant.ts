5f273c9261d8b2e5dd5417af78e7967ec20d85c8
Ibrahim <mdibrahimhaf6996@gmail.com>
2025-09-25 22:53:29 +0530

    added server url for render again.

--- client/src/constants/apiConstant.ts
+++ client/src/constants/apiConstant.ts
@@ -1,8 +1,8 @@
-// const MODE = import.meta.env.MODE;
+const MODE = import.meta.env.MODE;
 
-export const SERVER_URL = `http://localhost:4000/api/v1`;
+// export const SERVER_URL = `http://localhost:4000/api/v1`;
 
-// export const SERVER_URL =
-//     MODE === "development"
-//         ? `http://localhost:4000/api/v1`
-//         : `https://first-voice-dashboard-admin.onrender.com/api/v1`;
+export const SERVER_URL =
+    MODE === "development"
+        ? `http://localhost:4000/api/v1`
+        : `https://first-voice-dashboard-admin.onrender.com/api/v1`;
